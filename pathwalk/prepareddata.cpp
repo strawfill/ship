02b58d78062f17352645b62e56c9fecd7280c6c4
Aleksey Petrov <petrov.a@inftest.ru>
2021-10-21 10:40:47 +0300

    start for task 2

---
+++ pathwalk/prepareddata.cpp
@@ -0,0 +1,6 @@
+#include "prepareddata.h"
+
+PreparedData::PreparedData()
+{
+
+}
---
+++ pathwalk/prepareddata.h
@@ -0,0 +1,11 @@
+#ifndef PREPAREDDATA_H
+#define PREPAREDDATA_H
+
+
+class PreparedData
+{
+public:
+    PreparedData();
+};
+
+#endif // PREPAREDDATA_H
